bce65e3b2951d975c0cef68fd700cb59e76d08d8
김진언 <rlawlsdjs4582@gmail.com>
2026-02-08 23:52:24 +0900

    Update fake_lidar_node.cpp

--- autodrive/src/autodrive_practice/src/fake_lidar_node.cpp
+++ autodrive/src/autodrive_practice/src/fake_lidar_node.cpp
@@ -3,7 +3,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float32.hpp> // ros 표준 메세지 타입
 
-using namespace std::chrono_literals; // std::chrono::milliseconds(100) -> 100ms 표현 가능
+using namespace std::chrono_literals;
 
 class FakeLidarNode : public rclcpp::Node // rclcpp::Node를 상속 (클래스 자체가 ROS2 노드)
 {
@@ -18,27 +18,32 @@ public: // 외부에서 접근 가능
     this->declare_parameter<double>("min_m", 0.5);
     this->declare_parameter<double>("max_m", 20.0);
 
+    // parameter서버에서 값 읽어들이기
     topic_out_ = this->get_parameter("topic_out").as_string();
     rate_hz_   = this->get_parameter("rate_hz").as_double();
     min_m_     = this->get_parameter("min_m").as_double();
     max_m_     = this->get_parameter("max_m").as_double();
 
+    // publisher 생성 create_publisher<메세지 타입>(토픽이름, QoS)
+    // QoS(10) : 버퍼에 최대 10개 메세지 저장
     pub_ = this->create_publisher<std_msgs::msg::Float32>(topic_out_, rclcpp::QoS(10));
 
     // random
     std::random_device rd;
     rng_ = std::mt19937(rd());
+    // min과 max 사이의 균등분포
     dist_ = std::uniform_real_distribution<float>(static_cast<float>(min_m_), static_cast<float>(max_m_));
 
     auto period = std::chrono::duration<double>(1.0 / std::max(0.1, rate_hz_));
-    timer_ = this->create_wall_timer(
+    timer_ = this->create_wall_timer( // (주기, 콜백함수)
       std::chrono::duration_cast<std::chrono::milliseconds>(period),
       std::bind(&FakeLidarNode::on_timer, this));
 
     RCLCPP_INFO(get_logger(), "Publishing fake distance to %s (%.2f Hz, range %.2f~%.2f m)",
                 topic_out_.c_str(), rate_hz_, min_m_, max_m_);
   }
-
+// std::는 c++ 표준 라이브러리 표시
+// 다른 메서드와 혼동하지 않기 위함
 private:
   void on_timer()
   {
